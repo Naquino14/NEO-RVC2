4ce14fd8deffca8359b04bf0e987a33db88e7cd0
Nate Aquino <naquino14isaboss@gmail.com>
2026-01-03 13:52:26 -0500

    Clean up roles

--- app/src/roles.c
+++ app/src/roles.c
@@ -67,8 +67,6 @@ static const struct fs_mount_t sdcard_mnt_info = {
     .mnt_point = "/SD:"
 };
 
-// struct i2s_config i2s_cfg = {0};
-
 static role_devs_t m_role_devs = {
     .gpio_led0 = &led,
     .gpio_led0_stat = DEVSTAT_NOT_RDY,
@@ -194,29 +192,6 @@ static bool init_trc_i2s() {
         role_devs->dev_i2s_stat = DEVSTAT_ERR;
         return false;
     }
-
-    // const uint8_t channels = I2S_CHANNELS;
-    // const uint8_t word_size = I2S_WORD_SIZE_BYTES; /* bits */
-    // const uint32_t sample_rate = I2S_SAMPLE_RATE_HZ;
-    // const int NUM_BLOCKS = I2S_NUM_BLOCKS;
-    // const size_t BLOCK_SIZE = I2S_BLOCK_SIZE;
-
-    // /* Prepare i2s configuration */
-    // role_devs->i2s_cfg->word_size = word_size;
-    // role_devs->i2s_cfg->channels = channels;
-    // role_devs->i2s_cfg->format = I2S_FMT_DATA_FORMAT_I2S;
-    // role_devs->i2s_cfg->options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
-    // role_devs->i2s_cfg->frame_clk_freq = sample_rate;
-    // role_devs->i2s_cfg->mem_slab = &bit_tx_slab;
-    // role_devs->i2s_cfg->block_size = BLOCK_SIZE;
-    // role_devs->i2s_cfg->timeout = 250;
-
-    // ret = i2s_configure(role_devs->dev_i2s, I2S_DIR_TX, role_devs->i2s_cfg);
-    // if (ret < 0) {
-    //     LOG_ERR("I2S configure failed: %d", ret);
-    //     role_devs->dev_i2s_stat = DEVSTAT_ERR;
-    //     return false;
-    // }
 
     role_devs->dev_i2s_stat = DEVSTAT_RDY;
     LOG_INF("I2S\t\tRDY");
