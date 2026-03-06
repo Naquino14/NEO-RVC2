f5d53e1aacb395dda586d0b2216da426de8ad370
Nate Aquino <naquino14isaboss@gmail.com>
2026-03-05 22:53:14 -0500

    Create enums for message id and class, inpro and outpro values, and port identifiers

--- include/drivers/ufirebirdii.h
+++ include/drivers/ufirebirdii.h
@@ -1,6 +1,13 @@
+// Copyright (c) 2026 Nate Aquino
+// SPDX-License-Identifier: Apache-2.0
+//
+// See manual: https://en.unicore.com/uploads/file/UFirebirdII%20Series%20Protocol%20Specification_EN_R1.2.pdf
+
 #ifndef UFIREBIRDII_H
 #define UFIREBIRDII_H
 
+#include <stdint.h>
+#include <stdbool.h>
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
 
@@ -12,6 +19,101 @@ typedef struct {
     uint8_t satellites;
     bool valid;
 } ufirebirdii_fix_t;
+
+#define UFBII_ADDR_I2C 0x46
+#define UFBII_ADDR_DEFAULT 0x00
+
+enum ufirebirdii_port_id_t {
+    UFBII_PORT_ID_I2C   = 0x0,
+    UFBII_PORT_ID_UART1 = 0x1,
+    UFBII_PORT_ID_UART2 = 0x2,
+    UFBII_PORT_ID_SPI   = 0x4,
+};
+
+enum ufirebirdii_inpro {
+    UFBII_INPRO_UNICORE         = 0x0,      // DO NOT DISABLE (Page 27)
+    UFBII_INPRO_RTCM3_X         = 0x40,
+    UFBII_INPRO_MAPFB_ODODATA   = 0x400,
+};
+
+enum ufirebirdii_outpro  {
+    UFBII_OUTPRO_UNICORE        = 0x0,
+    UFBII_OUTPRO_NMEA           = 0x1, 
+    UFBII_OUTPRO_RTCM3_X        = 0x2,
+    UFBII_OUTPRO_NOTICE         = 0x10,
+    UFBII_OUTPRO_ERTCM4074DR    = 0x40,
+    UFBII_OUTPRO_ERTCM4074PVT   = 0x80,
+};
+
+enum ufirebirdii_msg_class {
+    UFBII_MSG_ID_NMEA_GGA = 0,
+    UFBII_MSG_ID_NMEA_GLL = 1,
+    UFBII_MSG_ID_NMEA_GSA = 2,
+    UFBII_MSG_ID_NMEA_GSV = 3,
+    UFBII_MSG_ID_NMEA_RMC = 4,
+    UFBII_MSG_ID_NMEA_VTG = 5,
+    UFBII_MSG_ID_NMEA_ZDA = 6,
+    UFBII_MSG_ID_NMEA_GST = 7,
+    UFBII_MSG_ID_NMEA_GBS = 8,
+    UFBII_MSG_ID_OBSERVATION_RTCM_MSM = 3,
+    UFBII_MSG_ID_OBSERVATION_RTCM_EPH = 4,
+    UFBII_MSG_ID_OBSERVATION_RTCM_STM = 5,
+    UFBII_MSG_ID_OBSERVATION_SYS_PARAM = 14,
+    UFBII_MSG_ID_SENSOR_FUSION_GYOACC = 0,
+    UFBII_MSG_ID_SENSOR_FUSION_SNRSTAT = 1,
+    UFBII_MSG_ID_SENSOR_FUSION_NAVATT = 2,
+    UFBII_MSG_ID_SENSOR_FUSION_IMURAW = 3,
+    UFBII_MSG_ID_SENSOR_FUSION_INSPVA = 4,
+    UFBII_MSG_ID_SENSOR_FUSION_IMUVEH = 5,
+    UFBII_MSG_ID_MISC_CWOUT = 0,
+    UFBII_MSG_ID_MISC_OSNMA = 1,
+    UFBII_MSG_ID_NOTICE_GENERAL = 0,
+    UFBII_MSG_ID_NOTICE_MESS_PKG = 1,
+    UFBII_MSG_ID_2ND_NMEA_GGA = 0,
+    UFBII_MSG_ID_2ND_NMEA_GLL = 1,
+    UFBII_MSG_ID_2ND_NMEA_GSA = 2,
+    UFBII_MSG_ID_2ND_NMEA_GSV = 3,
+    UFBII_MSG_ID_2ND_NMEA_RMC = 4,
+    UFBII_MSG_ID_2ND_NMEA_VTG = 5,
+    UFBII_MSG_ID_2ND_NMEA_ZDA = 6,
+    UFBII_MSG_ID_2ND_NMEA_GST = 7,
+    UFBII_MSG_ID_2ND_NMEA_GBS = 8,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_GYOACC = 0,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_SNRSTAT = 1,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_NAVATT = 2,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_IMURAW = 3,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_INSPVA = 4,
+    UFBII_MSG_ID_OBSERVATION_SENSOR_IMUVEH = 5,
+    UFBII_MSG_ID_OBSERVATION_RF_RX_INFO = 1,
+    UFBII_MSG_ID_OBSERVATION_RF_SIG_INFO = 2,
+    UFBII_MSG_ID_OBSERVATION_RF_TGD = 3,
+    UFBII_MSG_ID_OBSERVATION_RF_ION = 4,
+};
+
+enum ufirebirdii_msg_class_t {
+    UFBII_MSG_CLASS_NMEA = 0,
+    UFBII_MSG_CLASS_OBSERVATION = 2, // UM670A & UM680A & UM681A
+    UFBII_MSG_CLASS_SENSOR_FUSION = 4, // UM621 & UM681A
+    UFBII_MSG_CLASS_MISC = 5,
+    UFBII_MSG_CLASS_NOTICE = 6,
+    UFBII_MSG_CLASS_2ND_NMEA = 7, // UM621N
+    UFBII_MSG_CLASS_OBSERVATION_SENSOR = 8, // UM621 & UM681A
+    UFBII_MSG_CLASS_OBSERVATION_RF = 9, // UC6580 & UM670A & UM680A
+};
+
+typedef struct {
+    bool do_checksum;
+    // TODO
+} ufirebirdii_user_config_t;
+
+typedef struct {
+    uint32_t baud;
+    uint8_t addr;
+    enum ufirebirdii_inpro inpro;
+    enum ufirebirdii_outpro outpro;
+    ufirebirdii_user_config_t user_config;
+    // TODO
+} ufirebirdii_driver_config_t;
 
 struct ufirebirdii_api {
     int (*start)(const struct device* dev);
