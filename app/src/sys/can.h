c64feb18746b1b149a50e21136910fcba25107be
Nate Aquino <naquino14isaboss@gmail.com>
2026-05-20 13:39:17 -0400

    Remove all can code to start again from scratch

--- app/src/sys/can.h
+++ app/src/sys/can.h
@@ -4,12 +4,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/can.h>
 
-enum can_event_handler {
-    CEH_LOG,
-    CEH_SOUND,
-    CEH_LORA
-};
-
 int can_init();
 
 enum can_state get_can_state(const struct device* dev);
