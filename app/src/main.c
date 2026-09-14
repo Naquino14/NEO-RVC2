3cc00dd7de1f02f03ac5161324bad0f39612130a
Nate Aquino <naquino14isaboss@gmail.com>
2026-09-14 17:05:28 -0400

    Backup security lib 'test' in main

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,9 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/kernel.h>
 
+#include <nrvc2_security.h>
+#include <nrvc2_errno.h>
+
 #include "built-in-test.h"
 #include "roles.h"
 
@@ -18,6 +21,42 @@ int main(void) {
     }
     
     bit_basic();
+
+    // comms test
+    const char pt[] = "The quick brown fox jumped over the lazy dog!\n";
+    uint8_t ct[sizeof(pt)];
+    uint8_t pt_out[sizeof(pt)];
+    const keyopt_t keyopt = NRVC2_KEYOPT_SESS_COMMS_TRC2FOB;
+    uint8_t tag[NRVC2_SECURITY_TAG_SIZE];
+
+    // encryption test
+    int ret = nrvc2_security_encrypt_and_sign(keyopt, pt, sizeof(pt), ct, tag);
+    printk("encrypt and sign 1 returns %d\n", ret);
+
+    printk("sign 1: 0x");
+    for (uint8_t* tb = tag; tb < tag + sizeof(tag); tb++)
+        printk("%02x", *tb);
+    printk("\n");
+
+    printk("ct 1: 0x");
+    for (uint8_t* cb = ct; cb < ct + sizeof(ct); cb++)
+        printk("%02x", *cb);
+    printk("\n");
+
+    // decryption test
+    int seqn = 0;
+    ret = nrvc2_security_decrypt_and_verify(keyopt, ct, sizeof(ct), seqn, tag, pt_out);
+    if (ret == -EAUTH)
+        printk("Auth error!\n");
+    if (ret == -ESEQUENCE)
+        printk("Sequence number error!\n");
+
+    
+    printk("pt: '");
+    for (char* c = pt_out; c < pt_out + sizeof(pt_out); c++)
+        printk("%c", *c);
+    printk("'\n");
+
 
     return 0;
 }
